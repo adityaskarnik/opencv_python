27070aca122f20ea99ffd71c1ff979fa6c123fad
Aditya Karnik <aditya.s.karnik@gmail.com>
2021-05-02 17:47:05 +0530

    rgb

--- basic_image_operations.py
+++ basic_image_operations.py
@@ -9,13 +9,31 @@ import cv2
 # cv2.imwrite("output.jpg", img)
 
 # ch2_2 Accessing pixel data of an image
-img = cv2.imread("opencv-logo.png", 1)
-print(type(img)) # <class 'numpy.ndarray'>
-print(len(img)) # 739
-print(len(img[0])) # 600 - top row values, that is number of vertical columns
-print(len(img[0][0])) # 3 - number of channels found
-print(img.shape) # (739, 600, 3)
-print(img.dtype) # uint8 - unsigned integer of value 8 - 2**8
-print(img[10, 5]) # [255 255 255] - 10th row and 5th column values
-print(img[:, :, 0]) # all values containing first channel array in it
-print(img.size) # 1330200 - total number of pixels in the image
+# img = cv2.imread("opencv-logo.png", 1)
+# print(type(img)) # <class 'numpy.ndarray'>
+# print(len(img)) # 739
+# print(len(img[0])) # 600 - top row values, that is number of vertical columns
+# print(len(img[0][0])) # 3 - number of channels found
+# print(img.shape) # (739, 600, 3)
+# print(img.dtype) # uint8 - unsigned integer of value 8 - 2**8
+# print(img[10, 5]) # [255 255 255] - 10th row and 5th column values
+# print(img[:, :, 0]) # all values containing first channel array in it
+# print(img.size) # 1330200 - total number of pixels in the image
+
+# ch2_3 Matrix and Scalar operations
+black = np.zeros([150, 200, 1], 'uint8')
+cv2.imshow("Black", black)
+print(black[0,0,:])
+ones = np.ones([150, 200, 3], 'uint8')
+cv2.imshow("Ones", ones)
+print(ones[0,0,:])
+white = np.ones([150, 200, 3], 'uint16')
+white *= (2**16-1)
+cv2.imshow("White", white)
+print(white[0,0,:])
+color = ones.copy()
+color[:,:] = (255, 0, 0)
+cv2.imshow("Blues", color)
+print(color[0,0,:])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
